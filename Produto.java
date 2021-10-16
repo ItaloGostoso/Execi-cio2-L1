62a111f0fa17f4c377f0523c25646bb4301164f1
ItaloGostoso <89165297+ItaloGostoso@users.noreply.github.com>
2021-10-15 22:59:25 -0300

    Create Produto.java

---
+++ Produto.java
@@ -0,0 +1,35 @@
+package q4;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private int quantidade;
+    
+    public Produto(String nome, double preco, int quant){
+        this.nome = nm;
+        this.preco = prec;
+        this.quantidade = qut;
+        
+    }
+    public void comprar(int p){
+        if (p <= qut){
+            this.qut -= p;
+        }
+        else{
+            System.out.printf("Compra não efetuada.\n" + "Item indisponível. Quantidade" + " estoque: %d.\n", this.qut);
+        }
+    }
+
+    public String getnm() {
+        return nm;
+    }
+
+    public double getprec() {
+        return prec;
+    }
+
+    public int getqut() {
+        return qut;
+    }
+
+}
