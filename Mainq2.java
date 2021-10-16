370da38612cb4380ec9f7f23faace5fc071b25e5
ItaloGostoso <89165297+ItaloGostoso@users.noreply.github.com>
2021-10-15 22:19:28 -0300

    Create Mainq2.java

---
+++ Mainq2.java
@@ -0,0 +1,53 @@
+package q2;
+
+public class Post {
+   private String texto;
+   private String link;
+   private int numeroCurtidas;
+   private int numeroCompartilhamentos;
+   
+   public Post(String text, String link){
+       this.texto = txt;
+       this.link = lk;
+       
+   }
+   public void curtir() {
+       this.setnumeroCurtidas(this.getnumeroCurtidas() + 1);
+       
+   }
+   public void compartilhar(){
+       this.setnumeroCompartilhamentos(this.getnumeroCompartilhamentos() + 1);
+   }
+
+    public String gettxt() {
+        return txt;
+    }
+
+    public void settxt(String txt) {
+        this.texto = txt;
+    }
+
+    public String getlk() {
+        return lk;
+    }
+
+    public void setlk(String link) {
+        this.lk = lk;
+    }
+
+    public int getnumeroCurtidas() {
+        return numeroCurtidas;
+    }
+
+    public void setnumeroCurtidas(int numeroCurtidas) {
+        this.numeroCurtidas = numeroCurtidas;
+    }
+
+    public int getnumeroCompartilhamentos() {
+        return numeroCompartilhamentos;
+    }
+
+    public void setNumeroCompartilhamentos(int numeroCompartilhamentos) {
+        this.numeroCompartilhamentos = numeroCompartilhamentos;
+    }
+}
