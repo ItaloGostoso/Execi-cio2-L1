69dce34f8dd4d4c14db904c1810dafb8b244d332
ItaloGostoso <89165297+ItaloGostoso@users.noreply.github.com>
2021-10-15 23:05:11 -0300

    Create Mainq4.java

---
+++ Mainq4.java
@@ -0,0 +1,17 @@
+package q4;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Produto pelucia = new Produto("shrek", 5599, 34);
+        System.out.printf("Produto: %s\n Preço: %f\n", pelucia.getNome(),
+        pelucia.getPreco());
+       pelucia.comprar(3);
+        System.out.println("Quantidade em estoque após a compra: " + pelucia.getquantidade());
+        pelucia.comprar(5);
+        System.out.println("Quantidade em estoque após a compra: " + pelucia.getquantidade());
+        carro.comprar(10);
+        System.out.println("Quantidade em estoque (pós compra): " + pelucia.getquantidade());            
+    }
+    
+}
