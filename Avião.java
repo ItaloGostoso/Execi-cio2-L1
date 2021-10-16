22313ff5ff47434184cb54e1ff6824234a9f8a06
ItaloGostoso <89165297+ItaloGostoso@users.noreply.github.com>
2021-10-15 22:31:50 -0300

    Create Avião.java

---
+++ Avião.java
@@ -0,0 +1,28 @@
+package Q3;
+
+public class Aviao {
+    private int Altitude = 10;
+    private int Velocidade = 900;
+
+    
+    public Aviao(int altitude, int velocidade){
+        this.Altitude = alt;
+        this.Velocidade = vel;
+    }
+    public int getAltitude() {
+        return alt;
+    }
+
+    public void changeAltitude(int XX) {
+        this.altitude += XX;
+    }
+
+    public int getVelocidade() {
+        return vel;
+    }
+
+    public void changeVelocidade(int XX) {
+        this.velocidade += XX;
+    }
+
+}
